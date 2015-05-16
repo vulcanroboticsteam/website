c9549897d910753a676819e663201fef205f694a
Vulcan <vulcanroboticsteam@gmail.com>
2015-05-16 14:57:35 -0700

    Initial commit
    added all existing files

---
+++ Website Bootstrap Test/dist/js/highlightNav.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var str=location.href.toLowerCase();
+	console.log(str);
+	$('.navbar-nav li a').each(function() {
+	    if (str.indexOf(this.href.toLowerCase()) > -1) {
+			$("li.highlight").removeClass("highlight");
+	        $(this).parent().addClass("highlight"); 
+	    }
+    }); 
+	$('li.highlight').parents().each(function(){							  
+		if ($(this).is('li')){
+			$(this).addClass("highlight"); 
+		}							  
+	});
+})
